c9c6aa21da815126508bf3ba40a70774d951dccf
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-03-27 21:51:04 +0530

    change status endpoint response

--- src/main/java/com/example/FallBackController.java
+++ src/main/java/com/example/FallBackController.java
@@ -1,16 +1,21 @@
 package com.example;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class FallBackController {
 
     @GetMapping("/status/{status}")
-    public ResponseEntity<Integer> status(@PathVariable int status) {
-        return ResponseEntity.status(status).body(status);
+    public ResponseEntity<Map<String, Object>> status(@PathVariable int status) {
+        return ResponseEntity.status(status).body(Map.of(
+                "status", status, "message", HttpStatus.resolve(status).getReasonPhrase()
+        ));
     }
 
     @GetMapping("/fallback")
